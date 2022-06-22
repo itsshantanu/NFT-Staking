c2b2d79f5250942fe6c44749b87de010c600e055
Shantanu Bharadwaj <theshantanubharadwaj@gmail.com>
2022-06-22 18:31:15 +0530

    added getAPR function

--- contracts/MoonToken.sol
+++ contracts/MoonToken.sol
@@ -3,8 +3,34 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
-contract MoonToken is ERC20, Ownable {
+contract MoonToken is ERC20, Ownable, ERC1155Holder {
+    IERC1155 public nft;
+
+    struct stakeInfo{
+        uint256 tokenStakedAt;
+        uint256 stakeAmount; 
+        address tokenOwner;
+    }
+
+    mapping(uint256 => stakeInfo) public stakeInfos;
+
+    function getAPR(uint256 tokenId) public view returns(uint8){
+        uint256 time = stakeInfos[tokenId].tokenStakedAt;
+
+        if (block.timestamp - time < 30 days) {
+            return 0;
+        } else if (block.timestamp - time <  30 days * 6 ) {
+            return 10;
+        } else if (block.timestamp - time < 30 days * 12) {
+            return 15;
+        } else {
+            return 25;
+        }
+    }
+
     constructor() ERC20("MoonToken", "MOON") {}
 
     function mintToken(address sendTo, uint256 amountToSend) public onlyOwner {
