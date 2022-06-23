d3bc32a590e83084bf3ba19c4de1cf6f59587947
Shantanu Bharadwaj <theshantanubharadwaj@gmail.com>
2022-06-23 13:04:20 +0530

    made some changes in calculateTokens by adding isNFTStaked

--- contracts/StakeNFT.sol
+++ contracts/StakeNFT.sol
@@ -13,18 +13,19 @@ contract StakeNFT is ERC20, Ownable, ERC1155Holder {
         uint256 tokenStakedAt;
         uint256 stakeAmount; 
         address tokenOwner;
+        bool isNFTStaked;
     }
 
     mapping(uint256 => stakeInfo) public stakeInfos;
 
     function getAPR(uint256 tokenId) public view returns(uint8){
-        uint256 time = stakeInfos[tokenId].tokenStakedAt;
+        uint256 stakeTime = stakeInfos[tokenId].tokenStakedAt;
 
-        if (block.timestamp - time < 30 days) {
+        if (block.timestamp - stakeTime < 30 days) {
             return 0;
-        } else if (block.timestamp - time <  30 days * 6 ) {
+        } else if (block.timestamp - stakeTime <  30 days * 6 ) {
             return 10;
-        } else if (block.timestamp - time < 30 days * 12) {
+        } else if (block.timestamp - stakeTime < 30 days * 12) {
             return 15;
         } else {
             return 25;
@@ -37,20 +38,23 @@ contract StakeNFT is ERC20, Ownable, ERC1155Holder {
 
     function stake(uint256 tokenId, uint256 amount) external {
         require(nft.balanceOf(msg.sender, tokenId) >= amount, "You don't have enough balance to stake");
-        nft.safeTransferFrom(msg.sender, address(this), tokenId, amount, "0");
-        stakeInfos[tokenId] = stakeInfo(block.timestamp, amount, msg.sender);
+        nft.safeTransferFrom(msg.sender, address(this), tokenId, amount, "0x00");
+        stakeInfos[tokenId] = stakeInfo(block.timestamp, amount, msg.sender, true);
     }
 
     function calculateTokens(uint256 tokenId, uint256 amount) public view returns (uint256){
-        uint256 stakedTime = block.timestamp - stakeInfos[tokenId].tokenStakedAt;
+        stakeInfo memory readStakeInfo = stakeInfos[tokenId];
+        require(readStakeInfo.isNFTStaked == true, "NFT is not staked yet");
+        uint256 stakedTime = block.timestamp - readStakeInfo.tokenStakedAt;
         return getAPR(tokenId) * stakedTime * amount * 10 ** 18 /  (30 days * 12 * 100) ;
     } 
 
     function unstake(uint256 tokenId, uint256 amount) external {
         require(stakeInfos[tokenId].tokenOwner == msg.sender, "You can't unstake as you are not a owner");
         stakeInfos[tokenId].stakeAmount -= amount;
+        stakeInfos[tokenId].isNFTStaked = false;
         _mint(msg.sender, calculateTokens(tokenId, amount));
-        nft.safeTransferFrom(address(this), msg.sender, tokenId, amount, "0");
+        nft.safeTransferFrom(address(this), msg.sender, tokenId, amount, "0x00");
     }
 
     function burnToken(uint amount) external {
